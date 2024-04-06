b7c7ce50a426a3bf7e6a9565b4cd18088c6c76bd
Durvesh Wagh <durveshwagh11@gmail.com>
2024-04-06 17:38:35 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios"; 
+
+import './index.css'
+
+function App() {
+    const [advice, setAdvice] = useState('');
+
+    useEffect(() => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then((response) => {
+                const { advice } = response.data.slip;
+                setAdvice(advice); 
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []); 
+
+    return (
+        <div className="app">
+            <div className="card">
+                <h1 className="heading">{advice}</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App;
