ff05a5bf82391c95962932735278c801ad53e821
Durvesh Wagh <69915640+durveshwagh11@users.noreply.github.com>
2024-04-06 19:20:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,9 @@ function App() {
         <div className="app">
             <div className="card">
                 <h1 className="heading">{advice}</h1>
+                <button className= "button">
+                    <span>GIVE ME ADVICE</span>
+                </button>
             </div>
         </div>
     );
